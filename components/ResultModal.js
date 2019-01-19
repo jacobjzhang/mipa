b1a255aebd3e40bc7e21fc7eebadcae452ba058f
Jacob J. Zhang <jacobjzhang@gmail.com>
2019-01-19 16:58:11 -0500

    Clean up of the results modal

--- components/ResultModal.js
+++ components/ResultModal.js
@@ -4,7 +4,7 @@ import {
   getTheme
 } from 'react-native-material-kit';
 import Counter from 'react-native-counter';
-import { Card, Button } from 'react-native-elements';
+import { Card, Button, ListItem } from 'react-native-elements';
 
 const theme = getTheme();
 
@@ -25,7 +25,6 @@ class ResultModal extends React.Component {
   }  
 
   render() {
-
     return (
       <Modal
         animationType="slide"
@@ -37,29 +36,33 @@ class ResultModal extends React.Component {
         <View style={{
           position: 'absolute',
           bottom: 0,
-          backgroundColor: '#303952',
+          backgroundColor: '#26de81',
           paddingBottom: 20,
           width: '100%'
         }}>
           <View>
-            <Text style={{color: '#fff', fontSize: 20, textAlign: 'center', padding: 10}}>
-              <Text style={{color: '#fff', fontSize: 30, textAlign: 'center'}}>
-                Score: <Counter
+            <View style={{padding: 10}}>
+              <ListItem
+                key={this.props.currentResult}
+                title={this.props.currentResult}
+                leftIcon={{name: 'check', color: '#fff'}}
+              />{"\n"}
+              <Text style={{padding: 10, fontSize: 16}}>
+                SCORE: <Counter
                   end={this.props.score}                        // REQUIRED End of the counter
                   start={this.props.lastScore}                     // Beginning of the counter
                   time={1000}                   // Duration (in ms) of the counter
                   digits={0}                    // Number of digits after the comma
                   easing="linear"               // Easing function name
                 />
-              </Text>{"\n"}
-              {this.props.currentResult}
-            </Text>
+              </Text>
+            </View>
             <Button
               title="CONTINUE"
               onPress={() => this.props.goToNextQuestion()}
               style={{}}
               backgroundColor='#9D27B0'
-            />            
+            />
           </View>
         </View>          
       </Modal>
